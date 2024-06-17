b9111e54af40e8bea9753a798b6468cb4ffbad2d
Roel Bruggink <roel@jaroel.nl>
2024-06-17 21:07:52 +0200

    Rough insert block

--- src/components/blocks/page.tsx
+++ src/components/blocks/page.tsx
@@ -1,4 +1,4 @@
-import { Field, FieldArray, move, remove } from "@modular-forms/solid";
+import { Field, FieldArray, insert, move, remove } from "@modular-forms/solid";
 import { For } from "solid-js";
 import { EditBlock, ViewBlocks } from "~/components/Blocks";
 import type { BlockEditFormProps } from "../content/mapping";
@@ -138,6 +138,29 @@ export function EditPage(props: BlockEditFormProps) {
                               d="M2 7.5h16l-.811 7.71a2 2 0 0 1-1.99 1.79H4.802a2 2 0 0 1-1.99-1.79L2 7.5Zm5.22 1.72a.75.75 0 0 1 1.06 0L10 10.94l1.72-1.72a.75.75 0 1 1 1.06 1.06L11.06 12l1.72 1.72a.75.75 0 1 1-1.06 1.06L10 13.06l-1.72 1.72a.75.75 0 0 1-1.06-1.06L8.94 12l-1.72-1.72a.75.75 0 0 1 0-1.06Z"
                               clip-rule="evenodd"
                             />
+                          </svg>
+                        </button>
+                      </div>
+                      <div class="px-2">
+                        <button
+                          type="button"
+                          title="Insert new item above"
+                          class="size-4 disabled:text-gray-400"
+                          onClick={() => {
+                            insert(props.form, `${props.path}blocks`, {
+                              at: index(),
+                              value: { text: "", type: "text" },
+                            });
+                          }}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <title>Insert new item above</title>
+                            <path d="M9.25 13.25a.75.75 0 0 0 1.5 0V4.636l2.955 3.129a.75.75 0 0 0 1.09-1.03l-4.25-4.5a.75.75 0 0 0-1.09 0l-4.25 4.5a.75.75 0 1 0 1.09 1.03L9.25 4.636v8.614Z" />
+                            <path d="M3.5 12.75a.75.75 0 0 0-1.5 0v2.5A2.75 2.75 0 0 0 4.75 18h10.5A2.75 2.75 0 0 0 18 15.25v-2.5a.75.75 0 0 0-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5Z" />
                           </svg>
                         </button>
                       </div>
