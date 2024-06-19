a752d3de5fb5f9825148386853d33edcaa4f55ed
Roel Bruggink <roel@jaroel.nl>
2024-06-19 19:25:39 +0200

    - Extract svg Icons - Add toolbar to insert new block. Comes with nested blocks support.

---
+++ src/components/Icons.tsx
@@ -0,0 +1,95 @@
+export function DocumentIcon(props: { title: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <title>{props.title}</title>
+      <path
+        fill-rule="evenodd"
+        d="M4.5 2A1.5 1.5 0 0 0 3 3.5v13A1.5 1.5 0 0 0 4.5 18h11a1.5 1.5 0 0 0 1.5-1.5V7.621a1.5 1.5 0 0 0-.44-1.06l-4.12-4.122A1.5 1.5 0 0 0 11.378 2H4.5Zm2.25 8.5a.75.75 0 0 0 0 1.5h6.5a.75.75 0 0 0 0-1.5h-6.5Zm0 3a.75.75 0 0 0 0 1.5h6.5a.75.75 0 0 0 0-1.5h-6.5Z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export function RectangleStackIcon(props: { title: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <title>{props.title}</title>
+      <path d="M5.127 3.502 5.25 3.5h9.5c.041 0 .082 0 .123.002A2.251 2.251 0 0 0 12.75 2h-5.5a2.25 2.25 0 0 0-2.123 1.502ZM1 10.25A2.25 2.25 0 0 1 3.25 8h13.5A2.25 2.25 0 0 1 19 10.25v5.5A2.25 2.25 0 0 1 16.75 18H3.25A2.25 2.25 0 0 1 1 15.75v-5.5ZM3.25 6.5c-.04 0-.082 0-.123.002A2.25 2.25 0 0 1 5.25 5h9.5c.98 0 1.814.627 2.123 1.502a3.819 3.819 0 0 0-.123-.002H3.25Z" />
+    </svg>
+  );
+}
+
+export function ArrowUpTrayIcon(props: { title: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <title>{props.title}</title>
+      <path d="M9.25 13.25a.75.75 0 0 0 1.5 0V4.636l2.955 3.129a.75.75 0 0 0 1.09-1.03l-4.25-4.5a.75.75 0 0 0-1.09 0l-4.25 4.5a.75.75 0 1 0 1.09 1.03L9.25 4.636v8.614Z" />
+      <path d="M3.5 12.75a.75.75 0 0 0-1.5 0v2.5A2.75 2.75 0 0 0 4.75 18h10.5A2.75 2.75 0 0 0 18 15.25v-2.5a.75.75 0 0 0-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5Z" />
+    </svg>
+  );
+}
+
+export function ArchiveBoxXMarkIcon(props: { title: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <title>{props.title}</title>
+      <path d="M2 3a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H2Z" />
+      <path
+        fill-rule="evenodd"
+        d="M2 7.5h16l-.811 7.71a2 2 0 0 1-1.99 1.79H4.802a2 2 0 0 1-1.99-1.79L2 7.5Zm5.22 1.72a.75.75 0 0 1 1.06 0L10 10.94l1.72-1.72a.75.75 0 1 1 1.06 1.06L11.06 12l1.72 1.72a.75.75 0 1 1-1.06 1.06L10 13.06l-1.72 1.72a.75.75 0 0 1-1.06-1.06L8.94 12l-1.72-1.72a.75.75 0 0 1 0-1.06Z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export function ArrowDownIcon(props: { title: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <title>{props.title}</title>
+      <path
+        fill-rule="evenodd"
+        d="M10 3a.75.75 0 0 1 .75.75v10.638l3.96-4.158a.75.75 0 1 1 1.08 1.04l-5.25 5.5a.75.75 0 0 1-1.08 0l-5.25-5.5a.75.75 0 1 1 1.08-1.04l3.96 4.158V3.75A.75.75 0 0 1 10 3Z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export function ArrowUpIcon(props: { title: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <title>{props.title}</title>
+      <path
+        fill-rule="evenodd"
+        d="M10 17a.75.75 0 0 1-.75-.75V5.612L5.29 9.77a.75.75 0 0 1-1.08-1.04l5.25-5.5a.75.75 0 0 1 1.08 0l5.25 5.5a.75.75 0 1 1-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0 1 10 17Z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  );
+}
--- src/components/blocks/page.tsx
+++ src/components/blocks/page.tsx
@@ -1,6 +1,15 @@
+import { Button } from "@kobalte/core/button";
 import { Field, FieldArray, insert, move, remove } from "@modular-forms/solid";
 import { For } from "solid-js";
 import { EditBlock, ViewBlocks } from "~/components/Blocks";
+import {
+  ArchiveBoxXMarkIcon,
+  ArrowDownIcon,
+  ArrowUpIcon,
+  ArrowUpTrayIcon,
+  DocumentIcon,
+  RectangleStackIcon,
+} from "../Icons";
 import type { BlockEditFormProps } from "../content/mapping";
 import { TextField } from "../input/TextField";
 import type { PageBlock } from "./schemas";
@@ -64,7 +73,7 @@ export function EditPage(props: BlockEditFormProps) {
                   <div>
                     <div class="border-b border-orange-300 divide-x flex flex-row">
                       <div>
-                        <button
+                        <Button
                           type="button"
                           title="Move item up"
                           class="size-4 disabled:text-gray-400"
@@ -76,20 +85,9 @@ export function EditPage(props: BlockEditFormProps) {
                             });
                           }}
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <title>Move item up</title>
-                            <path
-                              fill-rule="evenodd"
-                              d="M10 17a.75.75 0 0 1-.75-.75V5.612L5.29 9.77a.75.75 0 0 1-1.08-1.04l5.25-5.5a.75.75 0 0 1 1.08 0l5.25 5.5a.75.75 0 1 1-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0 1 10 17Z"
-                              clip-rule="evenodd"
-                            />
-                          </svg>
-                        </button>
-                        <button
+                          <ArrowUpIcon title="Move item up" />
+                        </Button>
+                        <Button
                           type="button"
                           title="Move item down"
                           class="size-4 disabled:text-gray-400"
@@ -101,22 +99,11 @@ export function EditPage(props: BlockEditFormProps) {
                             });
                           }}
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <title>Move item down</title>
-                            <path
-                              fill-rule="evenodd"
-                              d="M10 3a.75.75 0 0 1 .75.75v10.638l3.96-4.158a.75.75 0 1 1 1.08 1.04l-5.25 5.5a.75.75 0 0 1-1.08 0l-5.25-5.5a.75.75 0 1 1 1.08-1.04l3.96 4.158V3.75A.75.75 0 0 1 10 3Z"
-                              clip-rule="evenodd"
-                            />
-                          </svg>
-                        </button>
+                          <ArrowDownIcon title="Move item down" />
+                        </Button>
                       </div>
                       <div class="px-2">
-                        <button
+                        <Button
                           type="button"
                           title="Delete item"
                           class="size-4 disabled:text-gray-400"
@@ -126,23 +113,11 @@ export function EditPage(props: BlockEditFormProps) {
                             });
                           }}
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <title>Remove this block</title>
-                            <path d="M2 3a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H2Z" />
-                            <path
-                              fill-rule="evenodd"
-                              d="M2 7.5h16l-.811 7.71a2 2 0 0 1-1.99 1.79H4.802a2 2 0 0 1-1.99-1.79L2 7.5Zm5.22 1.72a.75.75 0 0 1 1.06 0L10 10.94l1.72-1.72a.75.75 0 1 1 1.06 1.06L11.06 12l1.72 1.72a.75.75 0 1 1-1.06 1.06L10 13.06l-1.72 1.72a.75.75 0 0 1-1.06-1.06L8.94 12l-1.72-1.72a.75.75 0 0 1 0-1.06Z"
-                              clip-rule="evenodd"
-                            />
-                          </svg>
-                        </button>
+                          <ArchiveBoxXMarkIcon title="Remove this block" />
+                        </Button>
                       </div>
                       <div class="px-2">
-                        <button
+                        <Button
                           type="button"
                           title="Insert new item above"
                           class="size-4 disabled:text-gray-400"
@@ -153,16 +128,8 @@ export function EditPage(props: BlockEditFormProps) {
                             });
                           }}
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <title>Insert new item above</title>
-                            <path d="M9.25 13.25a.75.75 0 0 0 1.5 0V4.636l2.955 3.129a.75.75 0 0 0 1.09-1.03l-4.25-4.5a.75.75 0 0 0-1.09 0l-4.25 4.5a.75.75 0 1 0 1.09 1.03L9.25 4.636v8.614Z" />
-                            <path d="M3.5 12.75a.75.75 0 0 0-1.5 0v2.5A2.75 2.75 0 0 0 4.75 18h10.5A2.75 2.75 0 0 0 18 15.25v-2.5a.75.75 0 0 0-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5Z" />
-                          </svg>
-                        </button>
+                          <ArrowUpTrayIcon title="Insert new item above" />
+                        </Button>
                       </div>
                     </div>
                     <EditBlock
@@ -172,6 +139,41 @@ export function EditPage(props: BlockEditFormProps) {
                   </div>
                 )}
               </For>
+            </div>
+            <div>
+              <header>Insert a new block</header>
+              <div class="border-b border-orange-300 divide-x flex flex-row">
+                <div class="px-1">
+                  <Button
+                    type="button"
+                    title="Text block"
+                    class="size-4 disabled:text-gray-400"
+                    onClick={() => {
+                      insert(props.form, fieldArray.name, {
+                        at: fieldArray.items.length,
+                        value: { text: "", type: "text" },
+                      });
+                    }}
+                  >
+                    <DocumentIcon title="Insert text block" />
+                  </Button>
+                </div>
+                <div class="px-1">
+                  <Button
+                    type="button"
+                    title="Nested block"
+                    class="size-4 disabled:text-gray-400"
+                    onClick={() => {
+                      insert(props.form, fieldArray.name, {
+                        at: fieldArray.items.length,
+                        value: { type: "nested", nestedTitle: "" },
+                      });
+                    }}
+                  >
+                    <RectangleStackIcon title="Insert nested block" />
+                  </Button>
+                </div>
+              </div>
             </div>
           </div>
         )}
