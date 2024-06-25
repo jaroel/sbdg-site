832e3ef7d89f812a04741a70caf844ae3545e60d
Roel Bruggink <roel@jaroel.nl>
2024-06-25 23:12:56 +0200

    Use Tiptap editor for text blocks

--- src/components/blocks/text.tsx
+++ src/components/blocks/text.tsx
@@ -1,24 +1,82 @@
 import { Field } from "@modular-forms/solid";
-import { For } from "solid-js";
+import type { Component } from "solid-js";
+import { Dynamic, For, Show } from "solid-js/web";
+import TiptapEditor from "../TiptapEditor";
 import type { BlockEditFormProps } from "../content/mapping";
-import { TextField } from "../input/TextField";
-import type { TextBlock } from "./schemas";
+import type {
+  TextBlock,
+  TiptapDoc,
+  TiptapElement,
+  TiptapKeys,
+  TiptapParagraph,
+  TiptapText,
+} from "./schemas";
 
 export function EditText(props: BlockEditFormProps) {
   return (
     <>
       <Field of={props.form} name={`${props.path}text`}>
-        {(field, props) => (
-          <TextField
-            {...props}
-            label="Field text"
-            value={field.value}
-            error={field.error}
-            multiline
-          />
+        {(field, fprops) => (
+          <>
+            {field.error && <div class="text-red-500">{field.error}</div>}
+            <input
+              type="hidden"
+              name={field.name}
+              value={JSON.stringify(field.value)}
+            />
+            <TiptapEditor field={field} form={props.form} fprops={fprops} />
+          </>
         )}
       </Field>
     </>
+  );
+}
+
+function RenderText(props: {
+  element: TiptapText;
+}) {
+  return (
+    <Show when={props.element.marks} fallback={props.element.text}>
+      <strong>{props.element.text}</strong>
+    </Show>
+  );
+}
+
+function RenderParagraph(props: {
+  element: TiptapParagraph;
+}) {
+  return (
+    <For each={props.element.content}>
+      {(value) => (
+        <p>
+          <RenderTiptapElement element={value} />
+        </p>
+      )}
+    </For>
+  );
+}
+
+function renderDoc(props: {
+  element: TiptapDoc;
+}) {
+  return (
+    <For each={props.element.content}>
+      {(value) => <RenderTiptapElement element={value} />}
+    </For>
+  );
+}
+
+export const viewTiptapElement: Record<TiptapKeys, Component<any>> = {
+  text: RenderText,
+  doc: renderDoc,
+  paragraph: RenderParagraph,
+};
+
+function RenderTiptapElement(props: {
+  element: TiptapElement;
+}) {
+  return (
+    <Dynamic component={viewTiptapElement[props.element.type]} {...props} />
   );
 }
 
@@ -26,15 +84,8 @@ export default function ViewTextBlock(props: {
   object: TextBlock;
 }) {
   return (
-    <p class="text-sm text-gray-600">
-      <For each={props.object.text.split(/\r?\n/)}>
-        {(item) => (
-          <>
-            {item}
-            <br />
-          </>
-        )}
-      </For>
-    </p>
+    <>
+      <RenderTiptapElement element={props.object.text} />
+    </>
   );
 }
