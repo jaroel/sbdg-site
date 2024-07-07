46dc2ebbb17f5b6250a7240804fa4f9ad9494598
Roel Bruggink <roel@jaroel.nl>
2024-07-07 20:15:02 +0200

    Make ++file++ take the large object id from path

--- src/largeobject.ts
+++ src/largeobject.ts
@@ -7,14 +7,18 @@ import { db } from "./db/db";
 const beginSql: SingleSqlItem = { text: "BEGIN" };
 
 export async function streamLargeObject(event: HTTPEvent) {
-  const largeObjectId = 28392;
+  const largeObjectId = Number.parseInt(
+    event.path.replace("/++file++/", ""),
+    10,
+  );
 
   return await db.$adapter.transaction(beginSql, async (adapter) => {
     const loManager = new LargeObjectManager({
       pg: adapter.client,
     });
-    const [size, stream] =
-      await loManager.openAndReadableStreamAsync(largeObjectId);
+    const [size, stream] = await loManager.openAndReadableStreamAsync(
+      largeObjectId === 1 ? 28397 : largeObjectId,
+    );
     setResponseHeader(event, "content-length", size);
     await sendStream(event, stream);
   });
