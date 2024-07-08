52bd9bffb1c61ae208df59054660a11afb6d9552
Roel Bruggink <roel@jaroel.nl>
2024-07-08 20:18:20 +0200

    Add Select to Image block to select the oid to show

--- src/components/blocks/Image.tsx
+++ src/components/blocks/Image.tsx
@@ -1,9 +1,14 @@
 import { Field } from "@modular-forms/solid";
+import { Show, createEffect, createSignal } from "solid-js";
+import { listObjects } from "~/largeobject";
 import type { BlockEditFormProps } from "../content/mapping";
+import { Select } from "../input/Select";
 import { TextField } from "../input/TextField";
 import type { ImageBlock } from "./schemas";
 
 export function EditImage(props: BlockEditFormProps) {
+  const [oids, setOids] = createSignal<number[]>([]);
+  createEffect(async () => setOids(await listObjects()));
   return (
     <>
       <Field of={props.form} name={`${props.path}label`}>
@@ -26,17 +31,26 @@ export function EditImage(props: BlockEditFormProps) {
           />
         )}
       </Field>
-      <Field of={props.form} name={`${props.path}fileId`}>
-        {(field, fprops) => (
-          <TextField
-            {...fprops}
-            type="text"
-            label="fileId"
-            value={field.value}
-            error={field.error}
-          />
-        )}
-      </Field>
+      <Show when={oids().length}>
+        <Field of={props.form} name={`${props.path}fileId`}>
+          {(field, fprops) => (
+            <Select
+              {...fprops}
+              label="File Id"
+              placeholder={
+                (field.value && `Invalid value: ${field.value}`) ||
+                "Please select a value"
+              }
+              options={oids().map((oid) => {
+                return { label: `OID: ${oid}`, value: `id:${oid}` };
+              })}
+              value={field.value}
+              error={field.error}
+              class="max-w-fit"
+            />
+          )}
+        </Field>
+      </Show>
     </>
   );
 }
@@ -48,7 +62,11 @@ export default function ViewImageBlock(props: {
     <div class="ml-4">
       <p class="text-xl text-gray-600">{props.object.label}</p>
       {props.object.fileId && (
-        <img src={"/++file++/1"} class="w-[400px]" alt="Demo!" />
+        <img
+          src={`/++file++/${props.object.fileId}`}
+          class="w-[400px]"
+          alt="Demo!"
+        />
       )}
     </div>
   );
