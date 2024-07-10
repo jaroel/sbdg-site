38562e2be9e94317f6853ea27987a99c121a2337
Roel Bruggink <roel@jaroel.nl>
2024-07-10 20:28:59 +0200

    Add Dialog component

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,30 @@
+import { type DialogRootProps, Dialog as Kobalte } from "@kobalte/core/dialog";
+import "./Dialog.css";
+import type { ParentProps } from "solid-js";
+import { XMarkIcon } from "./Icons";
+
+export default function Dialog(
+  props: ParentProps & DialogRootProps & { title: string },
+) {
+  return (
+    <Kobalte {...props}>
+      <Kobalte.Trigger class="dialog__trigger">Open</Kobalte.Trigger>
+      <Kobalte.Portal>
+        <Kobalte.Overlay class="dialog__overlay" />
+        <div class="dialog__positioner">
+          <Kobalte.Content class="dialog__content">
+            <div class="dialog__header">
+              <Kobalte.Title class="dialog__title">{props.title}</Kobalte.Title>
+              <Kobalte.CloseButton class="dialog__close-button">
+                <XMarkIcon title="Close" />
+              </Kobalte.CloseButton>
+            </div>
+            <Kobalte.Description class="dialog__description">
+              {props.children}
+            </Kobalte.Description>
+          </Kobalte.Content>
+        </div>
+      </Kobalte.Portal>
+    </Kobalte>
+  );
+}
