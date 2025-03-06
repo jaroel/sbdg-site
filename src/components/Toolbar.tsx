a81007ab35bc5c908017b2734e038e14b5858ed0
Roel Bruggink <roel@jaroel.nl>
2025-03-06 23:01:04 +0100

    Show/hide object actions

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -5,34 +5,39 @@ export default function Toolbar(props: {
 }) {
   return (
     <div class="text-right bg-blue-100">
-      {props.item && (
-        <>
-          <a
-            class="mr-4"
-            href={`/delete${props.item.content.path}`}
-            title="Delete this item"
-          >
-            delete
-          </a>
-          <a
-            class="mr-4"
-            href={`/add${props.item.content.path}`}
-            title="Add a new item here"
-          >
-            add
-          </a>
-          <a
-            class="mr-4"
-            href={`/edit${props.item.content.path}`}
-            title="Edit this item"
-          >
-            edit
-          </a>
-          <a class="mr-4" href={props.item.content.path} title="View this item">
-            view
-          </a>
-        </>
-      )}
+      {props.item &&
+        (props.item?.content.object.status_code ?? 200) === 200 && (
+          <>
+            <a
+              class="mr-4"
+              href={`/delete${props.item.content.path}`}
+              title="Delete this item"
+            >
+              delete
+            </a>
+            <a
+              class="mr-4"
+              href={`/add${props.item.content.path}`}
+              title="Add a new item here"
+            >
+              add
+            </a>
+            <a
+              class="mr-4"
+              href={`/edit${props.item.content.path}`}
+              title="Edit this item"
+            >
+              edit
+            </a>
+            <a
+              class="mr-4"
+              href={props.item.content.path}
+              title="View this item"
+            >
+              view
+            </a>
+          </>
+        )}
       <a class="mr-4" href="/" title="Go to the home page">
         home
       </a>
