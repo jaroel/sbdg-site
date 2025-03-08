d16cb215290987730245843559cba3753d6676f3
Roel Bruggink <roel@jaroel.nl>
2025-03-09 00:10:32 +0100

    tailwind v4

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "@solidjs/start/config";
+import tailwindcss from "@tailwindcss/vite";
 import { imagetools } from "vite-imagetools";
 
 export default defineConfig({
   vite: {
-    plugins: [imagetools()],
+    plugins: [imagetools(), tailwindcss()],
     build: {
       reportCompressedSize: false,
     },
